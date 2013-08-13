7c6a5de69ed3c2f7b078d7713584b7b5b951528e
Karan Sharma <javakaran@live.com>
2013-08-13 11:38:08 +0530

    C# Hello world application

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("{0}", args[i]); 
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
